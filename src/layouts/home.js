4c80d2963527d5c2d2a68cf689d7812d5f6f0344
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 17:26:44 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -30,7 +30,7 @@ export default class Home extends React.Component {
                 </header>
                 {thumbImage && (
                     <Link className="post-thumbnail" href={postUrl}>
-                        <img className="thumbnail" src={withPrefix(thumbImage)} alt={thumbImageAlt} width="200px" />
+                        <img className="thumbnail" src={withPrefix(thumbImage)} alt={thumbImageAlt} width="100px" />
                     </Link>
                 )}
                 {excerpt && (
